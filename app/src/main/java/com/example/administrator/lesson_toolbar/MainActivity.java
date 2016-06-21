61d1e60d9b8722f5e8191f71ac1fa376b743dc40
HackerRiver <w123456789>
2016-06-21 15:38:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/lesson_toolbar/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.administrator.lesson_toolbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+getActionBar();
+
+        toolbar.setNavigationIcon(R.mipmap.ic_launcher);//设置导航栏图标
+        toolbar.setLogo(R.mipmap.ic_launcher);//设置app logo
+        toolbar.setTitle("Title");//设置主标题
+        toolbar.setSubtitle("Subtitle");//设置子标题
+        toolbar.inflateMenu(R.menu.menu_main);//设置右上角的填充菜单
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int menuItemId = item.getItemId();
+                if (menuItemId == R.id.action_search) {
+                    Toast.makeText(MainActivity.this, "action_search", Toast.LENGTH_SHORT).show();
+                } else if (menuItemId == R.id.action_notification) {
+                    Toast.makeText(MainActivity.this, "menu_notifications", Toast.LENGTH_SHORT).show();
+                } else if (menuItemId == R.id.action_item1) {
+                    Toast.makeText(MainActivity.this, "item_01", Toast.LENGTH_SHORT).show();
+                } else if (menuItemId == R.id.action_item2) {
+                    Toast.makeText(MainActivity.this, "item_02", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ app/src/main/java/com/example/administrator/lesson_toolbar/Person.java
@@ -0,0 +1,16 @@
+package com.example.administrator.lesson_toolbar;
+
+/**
+ * Created by Administrator on 2016/5/27.
+ */
+public class Person {
+
+    int age;
+    String dafs;
+
+    void show() {
+        System.out.print(dafs+age);
+        int age;
+    }
+
+}
